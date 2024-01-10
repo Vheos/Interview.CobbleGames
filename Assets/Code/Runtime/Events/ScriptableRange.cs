27784ca3250edd80c581303a2d36771024afdf76
Vheos <Vheos777@gmail.com>
2024-01-10 13:59:28 +0100

    Added SO script `ScriptableRange<T>`

---
+++ Assets/Code/Runtime/Events/ScriptableRange.cs
@@ -0,0 +1,14 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	public abstract class ScriptableRange<T> : ScriptableEvent
+	{
+		// Fields
+		[field: SerializeField] public T Min { get; private set; }
+		[field: SerializeField] public T Max { get; private set; }
+
+		// Methods
+		public abstract T Random { get; }
+	}
+}

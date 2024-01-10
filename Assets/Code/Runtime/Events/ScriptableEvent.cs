de4672db3f479915ecf7655375956b829ca9c9f3
Vheos <Vheos777@gmail.com>
2024-01-10 14:14:17 +0100

    Added SO script `ScriptableEvent`

---
+++ Assets/Code/Runtime/Events/ScriptableEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using System;
+	using UnityEngine;
+
+	public abstract class ScriptableEvent : ScriptableObject
+	{
+		// Fields
+		private Action action;
+
+		// Methods
+		public void Subscribe(Action action)
+			=> this.action += action;
+		public void Unsubscribe(Action action)
+			=> this.action -= action;
+		public void Invoke()
+			=> action?.Invoke();
+	}
+}

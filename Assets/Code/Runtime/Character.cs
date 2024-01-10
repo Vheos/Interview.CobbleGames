7793bca0b9ab66444252bd12aded74b1ffe97f9f
Vheos <Vheos777@gmail.com>
2024-01-10 18:23:38 +0100

    `Character`: updated movement logic

--- Assets/Code/Runtime/Character.cs
+++ Assets/Code/Runtime/Character.cs
@@ -1,33 +1,39 @@
 ﻿namespace Vheos.Interview.CobbleGames
 {
-	using System;
-	using System.Threading.Tasks;
 	using UnityEngine;
 
 	public class Character : MonoBehaviour
 	{
 		// Dependencies
 		[field: SerializeField] public CharacterCollector Collector { get; private set; }
-		[field: SerializeField] public FollowTarget FollowTarget { get; private set; }
-		[field: SerializeField] public LookAtTarget LookAtTarget { get; private set; }
+		[field: SerializeField] public MoveToPosition Mover { get; private set; }
+		[field: SerializeField] public MoveToTransform Follower { get; private set; }
 		[field: SerializeField] public CharacterAttributesRange AttributesRange { get; private set; }
 
 		// Fields
 		public CharacterAttributes Attributes { get; private set; }
 
 		// Methods
-		public void SetTarget(Transform target)
-			=> FollowTarget.Target = LookAtTarget.Target = target;
-		public Task MoveTo(Vector3 point)
-			=> throw new NotImplementedException();
+		public void Follow(Transform target)
+		{
+			Follower.Target = target;
+			Follower.enabled = true;
+			Mover.enabled = false;
+		}
+		public void MoveTo(Vector3 position)
+		{
+			Mover.Target = position;
+			Mover.enabled = true;
+			Follower.enabled = false;
+		}
 
 		// Unity
 		private void Awake()
 		{
 			Attributes = AttributesRange.Random;
+			Mover.Speed = Follower.Speed = Attributes.MoveSpeed;
 			Collector.Register(this);
 		}
-
 		private void OnDestroy()
 			=> Collector.Unregister(this);
 	}

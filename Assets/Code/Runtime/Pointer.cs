4f08bd26831f0cb8cfb67e16d7d05f3dcdbe8acf
Vheos <Vheos777@gmail.com>
2024-01-10 02:13:33 +0100

    Added UI component `Pointer`

---
+++ Assets/Code/Runtime/Pointer.cs
@@ -0,0 +1,37 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using System;
+	using UnityEngine;
+	using UnityEngine.InputSystem;
+
+	public class Pointer : MonoBehaviour, IClicker
+	{
+		// Dependencies
+		[field: SerializeField] public Camera Camera { get; private set; }
+		[field: SerializeField] public InputActionReference ClickAction { get; private set; }
+		[field: SerializeField] public InputActionReference Position { get; private set; }
+
+		// Events
+		public event Action OnClick;
+
+		// Methods
+		public Vector2 ScreenPosition
+			=> Position.action.ReadValue<Vector2>();
+		public void Click()
+		{
+#if DEBUG
+			Debug.Log($"Clicked at {ScreenPosition}");
+#endif
+			Camera.ScreenPointToRay(ScreenPosition);
+			OnClick?.Invoke();
+		}
+		private void Click(InputAction.CallbackContext _)
+			=> Click();
+
+		// Unity
+		private void OnEnable()
+			=> ClickAction.action.performed += Click;
+		private void OnDisable()
+			=> ClickAction.action.performed -= Click;
+	}
+}

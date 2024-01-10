11356859353a3be3825525112b1741bca9acd3a4
Vheos <Vheos777@gmail.com>
2024-01-10 14:14:44 +0100

    Added SO script and asset `PointerEvent`

---
+++ Assets/Code/Runtime/Events/PointerEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = nameof(PointerEvent), menuName = Const.AssetMenuPath + nameof(PointerEvent))]
+	public class PointerEvent : ScriptableEvent<Pointer>
+	{ }
+}

78a102ee2955a48693aebae17beab5335be8d1d6
Vheos <Vheos777@gmail.com>
2024-01-10 14:07:13 +0100

    Added component `LookAtTarget`

---
+++ Assets/Code/Runtime/LookAtTarget.cs
@@ -0,0 +1,25 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	public class LookAtTarget : MonoBehaviour
+	{
+		// Fields
+		[field: SerializeField] public Transform Target { get; set; }
+		[field: SerializeField, Range(0f, 10f)] public float Speed { get; private set; }
+
+		// Methods
+		private void TryLookAt()
+		{
+			if (Target == null || Target == transform)
+				return;
+
+			Vector3 direction = (Target.position - transform.position).normalized;
+			transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * Speed);
+		}
+
+		// Unity
+		private void Update()
+			=> TryLookAt();
+	}
+}

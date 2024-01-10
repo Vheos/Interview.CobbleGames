c2b65c52b02132e1bdd65548d77fdbc0fc281d87
Vheos <Vheos777@gmail.com>
2024-01-10 13:57:55 +0100

    Added class `CharacterAttributes`

---
+++ Assets/Code/Runtime/CharacterAttributes.cs
@@ -0,0 +1,14 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using System;
+	using UnityEngine;
+
+	[Serializable]
+	public class CharacterAttributes
+	{
+		// Fields
+		[Range(0.1f, 10f), Tooltip("Units per second")] public float MoveSpeed;
+		[Range(0.1f, 10f), Tooltip("Rotations per second")] public float TurnSpeed;
+		[Range(1, 100)] public int Health;
+	}
+}

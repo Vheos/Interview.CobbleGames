3f691074f9fe8bb8d6312c5e3a4bfb9e4470df48
Vheos <Vheos777@gmail.com>
2024-01-10 18:09:45 +0100

    Added component `LookInMoveDirection`

---
+++ Assets/Code/Runtime/Transform/LookInMoveDirection.cs
@@ -0,0 +1,28 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	public class LookInMoveDirection : MonoBehaviour
+	{
+		// Fields
+		[field: SerializeField, Range(0f, 10f)] public float Speed { get; private set; }
+		private Vector3 previousPosition;
+
+		// Methods
+		private void TryLook()
+		{
+			if (transform.position == previousPosition)
+				return;
+
+			Vector3 direction = (transform.position - previousPosition).normalized;
+			transform.forward = Vector3.Lerp(transform.forward, direction, Time.deltaTime * Speed);
+			previousPosition = transform.position;
+		}
+
+		// Unity
+		private void Start()
+			=> previousPosition = transform.position;
+		private void Update()
+			=> TryLook();
+	}
+}

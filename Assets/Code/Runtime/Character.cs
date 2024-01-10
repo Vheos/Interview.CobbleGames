31e0ba5f243a9911f2f47a2d31fbc1c5e483db1f
Vheos <Vheos777@gmail.com>
2024-01-10 01:26:17 +0100

    `Character`: added self un/registering

--- Assets/Code/Runtime/Character.cs
+++ Assets/Code/Runtime/Character.cs
@@ -3,5 +3,14 @@
 	using UnityEngine;
 
 	public class Character : MonoBehaviour
-	{ }
+	{
+		// Dependencies
+		[field: SerializeField] public CharacterCollector Collector { get; private set; }
+
+		// Unity
+		private void Awake() 
+			=> Collector.Register(this);
+		private void OnDestroy() 
+			=> Collector.Unregister(this);
+	}
 }

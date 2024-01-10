83608db891d692479b13d139aff3a76426863c38
Vheos <Vheos777@gmail.com>
2024-01-10 03:13:07 +0100

    Added component `FollowTarget`

---
+++ Assets/Code/Runtime/FollowTarget.cs
@@ -0,0 +1,33 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	public class FollowTarget : MonoBehaviour
+	{
+		// Fields
+		[field: SerializeField] public Transform Target { get; private set; }
+		[field: SerializeField, Range(0f, 10f)] public float MaxDistance { get; private set; }
+		[field: SerializeField, Range(0f, 10f)] public float MinDistance { get; private set; }
+		[field: SerializeField, Range(0f, 10f)] public float Speed { get; private set; }
+
+		// Methods
+		private void TryFollow()
+		{
+			if (Target == null || Target == transform)
+				return;
+
+			Vector3 offset = Target.position - transform.position;
+			float distance = offset.magnitude;
+			if (distance >= MinDistance && distance <= MaxDistance)
+				return;
+
+			Vector3 direction = offset.normalized;
+			Vector3 step = Time.deltaTime * Speed * direction;
+			transform.position += distance > MaxDistance ? step : -step;
+		}
+
+		// Unity
+		private void Update()
+			=> TryFollow();
+	}
+}

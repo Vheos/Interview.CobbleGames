3f1d061ff3e80fee12fcb5163074898a8ede13d6
Vheos <Vheos777@gmail.com>
2024-01-10 02:48:10 +0100

    Added static class `Extensions`

---
+++ Assets/Code/Runtime/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	public static class Extensions
+	{
+		public static int GetMask(this Layer @this)
+			=> LayerMask.GetMask(@this.ToString());
+	}
+}

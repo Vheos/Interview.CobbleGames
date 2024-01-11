c4c0fa6296a89b60f6d30992bc6d61e57aa3bdc8
Vheos <Vheos777@gmail.com>
2024-01-11 21:09:26 +0100

    Added character attribute `Color`

--- Assets/Code/Runtime/CharacterAttributes.cs
+++ Assets/Code/Runtime/CharacterAttributes.cs
@@ -10,5 +10,6 @@
 		[Range(0.1f, 10f), Tooltip("Units per second")] public float MoveSpeed;
 		[Range(0.1f, 10f), Tooltip("Rotations per second")] public float TurnSpeed;
 		[Range(1, 100)] public int Health;
+		public Color Color;
 	}
 }
--- Assets/Code/Runtime/Ranges/CharacterAttributesRange.cs
+++ Assets/Code/Runtime/Ranges/CharacterAttributesRange.cs
@@ -2,15 +2,25 @@
 {
 	using UnityEngine;
 
-	[CreateAssetMenu(fileName = nameof(CharacterAttributesRange), menuName = Const.AssetMenuPath + nameof(CharacterAttributesRange))]
+	[CreateAssetMenu(fileName = nameof(CharacterAttributesRange), menuName = AssetMenuPaths.Root + nameof(CharacterAttributesRange))]
 	public class CharacterAttributesRange : ScriptableRange<CharacterAttributes>
 	{
 		public override CharacterAttributes Random
-			=> new()
+		{
+			get
 			{
-				MoveSpeed = UnityEngine.Random.Range(Min.MoveSpeed, Max.MoveSpeed),
-				TurnSpeed = UnityEngine.Random.Range(Min.TurnSpeed, Max.TurnSpeed),
-				Health = UnityEngine.Random.Range(Min.Health, Max.Health),
-			};
+				Color.RGBToHSV(Min.Color, out var minH, out var minS, out var minV);
+				Color.RGBToHSV(Max.Color, out var maxH, out var maxS, out var maxV);
+				Color randomHSV = UnityEngine.Random.ColorHSV(minH, maxH, minS, maxS, minV, maxV, Min.Color.a, Max.Color.a);
+
+				return new()
+				{
+					MoveSpeed = UnityEngine.Random.Range(Min.MoveSpeed, Max.MoveSpeed),
+					TurnSpeed = UnityEngine.Random.Range(Min.TurnSpeed, Max.TurnSpeed),
+					Health = UnityEngine.Random.Range(Min.Health, Max.Health),
+					Color = randomHSV,
+				};
+			}
+		}
 	}
 }

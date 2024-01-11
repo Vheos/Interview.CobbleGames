67fa4cf853570876789363dbb7b741ae19c11e0b
Vheos <Vheos777@gmail.com>
2024-01-11 21:10:22 +0100

    Added SO script `CharacterEvent`

---
+++ Assets/Code/Runtime/Events/CharacterEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = nameof(CharacterEvent), menuName = AssetMenuPaths.Events + nameof(CharacterEvent))]
+	public class CharacterEvent : ScriptableEvent<Character>
+	{ }
+}

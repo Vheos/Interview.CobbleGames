a400b72e46b3c355ff4a64769c28df7571272c88
Vheos <Vheos777@gmail.com>
2024-01-14 18:04:45 +0100

    Added component and prefab `SaveableManager`

---
+++ Assets/Code/Runtime/SaveableManager.cs
@@ -0,0 +1,36 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+	using Newtonsoft.Json;
+	using System.IO;
+	using System.Collections.Generic;
+
+	public class SaveableManager : MonoBehaviour
+	{
+		// Dependencies
+		[field: SerializeField] public SaveableCollector SaveableCollector { get; private set; }
+
+		// Fields
+		[field: SerializeField] public FileHandler<SaveableData> FileHandler { get; private set; }
+		private bool isBusy;
+
+		// Methods
+		[ContextMenu(nameof(Save))]
+		public async void Save()
+		{
+			SaveableData data = new();
+			SaveableCollector.Items.DoForEach(t => t.SaveData(data));
+			await FileHandler.WriteAsync(data);
+
+			Debug.Log("Save successful");
+		}
+		[ContextMenu(nameof(Load))]
+		public async void Load()
+		{
+			SaveableData data = await FileHandler.ReadAsync();
+			SaveableCollector.Items.DoForEach(t => t.LoadData(data));
+
+			Debug.Log("Load successful");
+		}
+	}
+}

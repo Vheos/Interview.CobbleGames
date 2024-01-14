602bf81d242b7f194477a0720cd27d12ac8d134e
Vheos <Vheos777@gmail.com>
2024-01-14 18:02:56 +0100

    Added class `JsonSerializer`

---
+++ Assets/Code/Runtime/JsonSerializer.cs
@@ -0,0 +1,22 @@
+namespace Vheos.Interview.CobbleGames
+{
+	using Newtonsoft.Json;
+
+	public class JsonSerializer : ISerializer<string>
+	{
+		// Fields
+		private readonly JsonSerializerSettings settings;
+
+		// Constuctors
+		public JsonSerializer(JsonSerializerSettings settings)
+		{
+			this.settings = settings;
+		}
+
+		// Methods
+		public string Serialize<T>(T toSerialize)
+			=> JsonConvert.SerializeObject(toSerialize, settings);
+		public T Deserialize<T>(string json)
+			=> JsonConvert.DeserializeObject<T>(json, settings);
+	}
+}

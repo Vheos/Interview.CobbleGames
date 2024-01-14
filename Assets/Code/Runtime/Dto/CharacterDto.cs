75b9ad1aa9e79d3366bfbf715aad1a07f35f69a6
Vheos <Vheos777@gmail.com>
2024-01-14 17:59:04 +0100

    Added dto struct `CharacterDto`

---
+++ Assets/Code/Runtime/Dto/CharacterDto.cs
@@ -0,0 +1,22 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	public struct CharacterDto
+	{
+		// Fields
+		public Vector3Dto Position;
+		public float MoveSpeed;
+		public float TurnSpeed;
+		public int Health;
+		public ColorDto Color;
+
+		// Constructors
+		public CharacterDto(Character character)
+		{
+			Position = new(character.transform.position);
+			MoveSpeed = character.Attributes.MoveSpeed;
+			TurnSpeed = character.Attributes.TurnSpeed;
+			Health = character.Attributes.Health;
+			Color = new(character.Attributes.Color);
+		}
+	}
+}

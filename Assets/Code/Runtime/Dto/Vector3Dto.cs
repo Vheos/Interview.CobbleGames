aa1a61c8e075e12ce934ebc54a0c9329abd3ec7a
Vheos <Vheos777@gmail.com>
2024-01-14 17:58:44 +0100

    Added dto struct `Vector3Dto`

---
+++ Assets/Code/Runtime/Dto/Vector3Dto.cs
@@ -0,0 +1,24 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using Newtonsoft.Json;
+	using UnityEngine;
+
+	public struct Vector3Dto
+	{
+		// Fields
+		public float X, Y, Z;
+
+		// Methods
+		[JsonIgnore]
+		public readonly Vector3 UnityVector3
+			=> new(X, Y, Z);
+
+		// Constructors
+		public Vector3Dto(Vector3 vector3)
+		{
+			X = vector3.x;
+			Y = vector3.y;
+			Z = vector3.z;
+		}
+	}
+}

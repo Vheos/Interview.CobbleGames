d8160c9f2eb791e44051ef92b5617d38dd9eaed9
Vheos <Vheos777@gmail.com>
2024-01-14 18:03:15 +0100

    Added class `FileHandler`

---
+++ Assets/Code/Runtime/FileHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+	using Newtonsoft.Json;
+	using System.IO;
+	using System.Threading.Tasks;
+	using System;
+
+	[Serializable]
+	public class FileHandler<T>
+	{
+		// Fields
+		[field: SerializeField] public string FileName { get; private set; }
+		[field: SerializeField] public string FileExtension { get; private set; }
+		[field: SerializeField] public string FolderPathOverride { get; private set; }
+		private static readonly JsonSerializerSettings serializerSettings = new()
+		{
+			TypeNameHandling = TypeNameHandling.Auto,
+			Formatting = Formatting.Indented
+		};
+		private readonly ISerializer<string> serializer = new JsonSerializer(serializerSettings);
+
+		// Methods
+		private string FolderPath
+			=> string.IsNullOrEmpty(FolderPathOverride) ? Application.persistentDataPath : FolderPathOverride;
+		private string FullPath
+			=> Path.Combine(FolderPath, Path.ChangeExtension(FileName, FileExtension));
+		public async Task WriteAsync(T data)
+		{
+			string dataJson = serializer.Serialize(data);
+			await File.WriteAllTextAsync(FullPath, dataJson);
+		}
+		public async Task<T> ReadAsync()
+		{
+			string dataJson = await File.ReadAllTextAsync(FullPath);
+			return serializer.Deserialize<T>(dataJson);
+		}
+	}
+}

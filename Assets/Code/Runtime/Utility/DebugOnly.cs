f2e6d50f47535ad5c86dccc9bed1985ceed5241f
Vheos <Vheos777@gmail.com>
2024-01-14 20:06:00 +0100

    Added component `DebugOnly`

---
+++ Assets/Code/Runtime/Utility/DebugOnly.cs
@@ -0,0 +1,65 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	public class DebugOnly : MonoBehaviour
+	{
+		// Fields
+		[SerializeField] private Action action;
+		[SerializeField] private ActionTiming timing;
+
+		// Methods
+		private void PerformAction()
+		{
+			switch (action)
+			{
+				case Action.DestroyImmediate:
+					DestroyImmediate(gameObject);
+					break;
+				case Action.Destroy:
+					Destroy(gameObject);
+					break;
+				case Action.InactivateGameObject:
+					gameObject.SetActive(false);
+					break;
+				case Action.DisableMonoBehaviours:
+					GetComponents<MonoBehaviour>().DoForEach(mb => mb.enabled = false);
+					break;
+			}
+		}
+
+		// Unity
+#if !DEBUG
+		private void Awake()
+		{
+			if (timing == ActionTiming.Awake)
+				PerformAction();
+		}
+		private void OnEnable()
+		{
+			if (timing == ActionTiming.OnEnable)
+				PerformAction();
+		}
+		private void Start()
+		{
+			if (timing == ActionTiming.Start)
+				PerformAction();
+		}
+#endif
+
+		// Definitions
+		private enum Action
+		{
+			DestroyImmediate,
+			Destroy,
+			InactivateGameObject,
+			DisableMonoBehaviours,
+		}
+		private enum ActionTiming
+		{
+			Awake,
+			OnEnable,
+			Start,
+		}
+	}
+}

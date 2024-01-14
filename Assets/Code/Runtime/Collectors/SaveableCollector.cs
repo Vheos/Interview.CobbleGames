5bbe393063fc2b98d31529a248bae6ab6d470882
Vheos <Vheos777@gmail.com>
2024-01-14 17:48:01 +0100

    Added SO script and asset `SaveableCollector`

---
+++ Assets/Code/Runtime/Collectors/SaveableCollector.cs
@@ -0,0 +1,8 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = nameof(SaveableCollector), menuName = AssetMenuPaths.Root + nameof(SaveableCollector))]
+	public class SaveableCollector : ScriptableCollector<ISaveable>
+	{ }
+}

67361708fc34a53e3c15d28e82a9902e6bb42bf3
Vheos <Vheos777@gmail.com>
2024-01-16 17:06:34 +0100

    Added SO script and asset `TransformRange`

---
+++ Assets/Code/Runtime/Ranges/TransformRange.cs
@@ -0,0 +1,23 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using UnityEngine;
+
+	[CreateAssetMenu(fileName = nameof(TransformRange), menuName = AssetMenuPaths.Ranges + nameof(TransformRange))]
+	public class TransformRange : ScriptableRange<TransformDto>
+	{
+		public override TransformDto Random
+			=> new()
+			{
+				Position = RandomVector(Min.Position, Max.Position),
+				EulerAngles = RandomVector(Min.EulerAngles, Max.EulerAngles),
+				Scale = RandomVector(Min.Scale, Max.Scale),
+			};
+		private Vector3Dto RandomVector(Vector3Dto from, Vector3Dto to)
+			=> new()
+			{
+				X = UnityEngine.Random.Range(from.X, to.X),
+				Y = UnityEngine.Random.Range(from.Y, to.Y),
+				Z = UnityEngine.Random.Range(from.Z, to.Z),
+			};
+	}
+}

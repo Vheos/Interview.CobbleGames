512e214e82b72d7de484c246dfb8d5ed688f0dc9
Vheos <Vheos777@gmail.com>
2024-01-16 17:12:42 +0100

    Added class `NodeInfo`

---
+++ Assets/Code/Runtime/Pathfinding/NodeInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace Vheos.Interview.CobbleGames
+{
+	using System;
+
+	public class NodeInfo
+	{
+		// Fields
+		public readonly Node Node;
+		public readonly float HeuristicCost;
+		public NodeInfo Previous;
+		public float MoveCost;
+		public bool AlreadyTraversed;
+
+		// Constructors
+		public NodeInfo(Node node, Node endNode, Func<Node, Node, float> heuristicFunc)
+		{
+			Node = node;
+			HeuristicCost = heuristicFunc(node, endNode);
+		}
+
+		// Methods
+		public float TotalCost
+			=> MoveCost + HeuristicCost;
+		public string DebugText
+			=> $"{Node.Id}   /   {MoveCost:F1} + {HeuristicCost:F1} = {TotalCost:F1}";
+
+	}
+}
